d41e009741889feb59e332fd67604760495b5442
shraay <sarahdelmas17@hotmail.com>
2018-04-16 17:45:42 +0200

    maj fsm

--- src/FSM/src/fsm.cpp
+++ src/FSM/src/fsm.cpp
@@ -19,12 +19,15 @@
 #include <tf/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
 
+#define ERR_DISTANCE 5
+
 
 float x_helios, y_helios;
 float pi = 3.1415926535;
 std::vector<geometry_msgs::PoseStamped> msg_waypoints;
 std::array<std::array<float, 2>, 2> ab;// = { {ax, bx},{ay, by} };
 std::array<std::array<float, 2>, 1> helios;
+std::array<std::array<float, 2>, 1> d12, d2;
 
 
 void commandeCallback(const nav_msgs::Path::ConstPtr& msg_path){
@@ -35,24 +38,19 @@ void commandeCallback(const nav_msgs::Path::ConstPtr& msg_path){
 void vectXCallback(const geometry_msgs::PoseStamped::ConstPtr& msg_helios){
   x_helios = msg_helios->pose.position.x;
   y_helios = msg_helios->pose.position.y;
-  helios[0,0] = x_helios;
-  helios[1,0] = y_helios;
+  helios[0][0] = x_helios;
+  helios[1][0] = y_helios;
 }
 
 int main(int argc, char **argv){
-    float ax;
-    float ay;
-    float bx;
-    float by;
-    float det;
     std::array<std::array<float, 2>, 2> ab;// = { {ax, bx},{ay, by} };
     std::array<std::array<float, 2>, 1> helios;
 
     float x = 0;
     float y = 0;
     int lenght_wpts;
-    float theta, command, e;
-    float d1, d2, d;
+    float theta, command;
+    float e;
     int i = 0;
 
     ros::init(argc, argv, "FSM");
@@ -64,7 +62,7 @@ int main(int argc, char **argv){
 
     //Publie sur
     ros::Publisher etats_pub = n.advertise<std_msgs::String>("Etats", 1);
-    ros::Publisher direction_pub = n.advertise<geometry_msgs:PoseArray>("Pose_Direction", 1);
+    ros::Publisher direction_pub = n.advertise<geometry_msgs::PoseArray>("Pose_Direction", 1);
     ros::Publisher position_pub = n.advertise<std_msgs::Float64>("Path_Position", 1);
     ros::Publisher num_wpt_pub = n.advertise<std_msgs::Int64>("Numero", 1);
     ros::Rate loop_rate(20);
@@ -72,37 +70,44 @@ int main(int argc, char **argv){
     while(ros::ok()){
         lenght_wpts = msg_waypoints.size();
 
-        for (i = 0; i<lenght_wpts-1; i++) {
-            /*
-            ab[0,0] = msg_waypoints->pose[i].x;
-            ab[1,0] = msg_waypoints->pose[i].y;
-            ab[0,1] = msg_waypoints->pose[i + 1].x;
-            ab[1,1] = msg_waypoints->pose[i + 1].y;
-            */
+/*
+        ab[0,0] = msg_waypoints[i].pose.position.x;
+        ab[1,0] = msg_waypoints[i].pose.position.y;
+        ab[0,1] = msg_waypoints[i+1].pose.position.x;
+        ab[1,1] = msg_waypoints[i+1].pose.position.y;
+*/
+        ab[0][0] = 5;
+        ab[1][0] = 3;
+        ab[0][1] = 10;
+        ab[1][1] = 3;
+        helios[0][0] = 7;
+        helios[1][0] = 6;
 
-            ab[0,0] = 5;
-            ab[1,0] = 3;
-            ab[0,1] = 10;
-            ab[1,1] = 3;
-            helios[0,0] = 7;
-            helios[1,0] = 6;
+        geometry_msgs::PoseArray msg_ab;
+        msg_ab.header.stamp = ros::Time::now();
+        msg_ab.poses[0].position.x = ab[0][0];
+        msg_ab.poses[0].position.y = ab[1][0];
+        msg_ab.poses[1].position.x = ab[0][1];
+        msg_ab.poses[1].position.y = ab[1][1];
 
-            geometry_msgs::poseArray msg_ab;
-            msg_ab.header.stamp = ros::Time::now();
-            msg_ab.pose[0].x = ab[0,0];
-            msg_ab.pose[0].y = ab[1,0];
-            msg_ab.pose[1].x = ab[0,1];
-            msg_ab.pose[1].y = ab[1,1];
+        e = (ab[0][1] - ab[0][0])*(helios[1][0] - ab[1][0]) -
+            (ab[1][1] - ab[1][0])*(helios[0][0] - ab[0][0]);
 
-            d1 = ab[:, 1] - ab[:, 0];
-            d2 = helios - ab[:, 0];
-            e = d1[0, 0]*d2[1, 0] - d1[1, 0]*d2[0, 0]
+        //passage aux pts suivants
+        if (e < ERR_DISTANCE){
+          i++;
+        }
+        //fini de lire tous les wayoints
+        /*
+        if (i > lenght_wpts-1) {
+          x= 0;
+        } // fin if
+        */
 
-            printf("e %f", e);
-
-
-        std_msgs::Int64 msg_etat = "none";
-        std_msgs::Int64 msg_num_wpts = 0;
+        std_msgs::String msg_etat;
+        msg_etat.data = "none";
+        std_msgs::Int64 msg_num_wpts;
+        msg_num_wpts.data = i++;
 
         //Publication des messages
         direction_pub.publish(msg_ab);
@@ -111,6 +116,6 @@ int main(int argc, char **argv){
         ros::spinOnce();
         loop_rate.sleep();
 
-    }
+    } // fin while
     return 0;
 }
